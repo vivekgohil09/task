35d9c5b42a98a7ff75fa24a9050f25a034258b93
vivekgohil09 <vvekgohil@gmail.com>
2024-07-14 15:42:59 +0530

    add files

--- sevatask/src/App.jsx
+++ sevatask/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from '@material-tailwind/react'
+
+
+import { Login } from './Components/Login.jsx'
+
 
 function App() {
 
   return (
     <>
-    
+    <Login/>
     </>
   )
 }
